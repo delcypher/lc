2b23fbc145c3b8837a96992f911a693f5b6b7da7
Alex Allen <aa7086@bristol.ac.uk>
2011-01-20 13:48:07 +0000

    Updated device-probe to give more info and not use printf

--- cuda-tools/device_probe.cu
+++ cuda-tools/device_probe.cu
@@ -1,5 +1,6 @@
-#include <stdio.h>
+#include <iostream>
 #include <cuda_runtime.h>
+using namespace std;
 
 /* This small program is used to probe currently available CUDA devices
 *  and lists their computer capability.
@@ -14,14 +15,20 @@ int main()
 
 	cudaGetDeviceCount(&deviceCount);
 	
-	printf("Found %d CUDA devices.\n", deviceCount);
+	cout << "\nFound " << deviceCount << " CUDA devices.\n\n";
 
 	for(counter=0; counter < deviceCount; counter++)
 	{
 		cudaGetDeviceProperties(&deviceProperties, counter);
-		printf("[%d]Name: %s\n",counter, deviceProperties.name);
-		printf("[%d]Compute capability: %d.%d\n", counter, deviceProperties.major, deviceProperties.minor);
-		printf("\n\n");
+		cout << "Device " << counter +1 << ":\n";
+		cout << "Name: " << deviceProperties.name << endl;
+		cout << "Compute capability: " << deviceProperties.major << "." << deviceProperties.minor << endl;
+		cout << "Multiprocessor count: " << deviceProperties.multiProcessorCount << endl;
+		cout << "Maximum threads per block: " << deviceProperties.maxThreadsPerBlock << endl;
+		cout << "Total device memory (MiB): " << (double) deviceProperties.totalGlobalMem / (1024*1024) << endl;
+		cout << "Maximum shared memory per block (KiB): " << (double) deviceProperties.sharedMemPerBlock / 1024 << endl;
+		cout << "Maximum constant memory (KiB): " << (double) deviceProperties.totalConstMem / 1024 << endl;
+		cout << "\n\n";
 
 	}
 	
