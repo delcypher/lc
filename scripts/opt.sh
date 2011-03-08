51c3dc91f9612d895ff573fb60a2c753fd49d3ff
Dan Liew <delcypher@gmail.com>
2011-03-08 10:22:12 +0000

    * Added opt.sh bash script to automate timing runs for optimisation.

---
+++ scripts/opt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+#Small helper script to time program run time.
+
+#Assume $1 is no of monte carlo steps
+MCS=$1
+
+
+for ((i=0; i<10; i++)); do
+	echo "## $i"
+	time -p ./sim-state test.bin $MCS 1> /dev/null
+	echo "##"
+done;
